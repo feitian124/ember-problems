935bb4f0f3afd85f81f2abd49c72a738b5b90ac4
feitian124 <feitian124@gmail.com>
2014-11-27 14:30:04 +0800

    show and mock posts

--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -1,8 +1,22 @@
 module.exports = function(app) {
   var express = require('express');
   var postsRouter = express.Router();
+
   postsRouter.get('/', function(req, res) {
-    res.send({"posts":[]});
+    res.send({
+      "posts": fakePosts(10)
+    });
   });
   app.use('/api/posts', postsRouter);
 };
+
+function fakePosts(n) {
+  var i = 0, posts = [];
+
+  while(i < n) {
+    posts.push({id: i, text: 'post' + i});
+    i++;
+  }
+
+  return posts;
+}
